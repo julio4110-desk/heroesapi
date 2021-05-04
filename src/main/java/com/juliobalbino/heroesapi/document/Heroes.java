2d55eba359fe9291bbc99981276a9409d2ba4953
Julio Balbino <julio4110@hotmail.com>
2021-05-04 15:30:25 -0300

    Controller, Repository, Service criados

---
+++ src/main/java/com/juliobalbino/heroesapi/document/Heroes.java
@@ -0,0 +1,29 @@
+package com.juliobalbino.heroesapi.document;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nonapi.io.github.classgraph.json.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamoDBTable(tableName = "Heroes_Table")
+public class Heroes {
+
+    @Id
+    @DynamoDBHashKey(attributeName = "id")
+    private String id;
+
+    @DynamoDBAttribute(attributeName = "name")
+    private String name;
+
+    @DynamoDBAttribute(attributeName = "universe")
+    private String universe;
+
+    @DynamoDBAttribute(attributeName = "films")
+    private String films;
+}
